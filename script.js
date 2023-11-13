a8a81a5d04da4325e4beb5ad6d608ac08fba9fac
Alexander <super.sany96@yandex.ru>
2023-11-13 11:49:46 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+let form = document.querySelector('form');
+let inputs = document.querySelectorAll('.form-input');
+
+// Сохраненные данные из localStorage
+loadFormData();
+
+// Обработчик события отправки формы
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  if (validateForm()) {
+    saveFormData();
+    sendFormData();
+  }
+});
+
+// Проверка полей формы
+function validateForm() {
+  for (let i = 0; i < inputs.length; i++) {
+    let input = inputs[i];
+    if (input.value.trim() === '') {
+      alert('Пожалуйста, заполните все обязательные поля');
+      return false;
+    }
+  }
+  return true;
+}
+
+// Сохранение данных формы в localStorage
+function saveFormData() {
+  let formData = {};
+  for (let i = 0; i < inputs.length; i++) {
+    const input = inputs[i];
+    formData[input.name] = input.value;
+  }
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+// Отправка данных формы на почту
+function sendFormData() {
+  form.action = 'mail.php';
+  form.method = 'POST';
+  form.submit();
+}
+
+// Загрузка данных формы из localStorage
+function loadFormData() {
+  const savedFormData = localStorage.getItem('formData');
+  if (savedFormData) {
+    let formData = JSON.parse(savedFormData);
+    for (let i = 0; i < inputs.length; i++) {
+      const input = inputs[i];
+      if (formData[input.name]) {
+        input.value = formData[input.name];
+      }
+    }
+  }
+}
+
+// Применение маски серии паспорта
+let seriyaInput = document.querySelector('.form-input-seriy');
+Inputmask('999-999').mask(seriyaInput);
+
+// Применение маски номера паспорта
+let numberInput = document.querySelector('.form-input-number');
+Inputmask('99-99 999999').mask(numberInput);
